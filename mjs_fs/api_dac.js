dd8ead30497a01254a74e9f086cfdfe120204e08
Francesco Colasante <frank_997@hotmail.it>
2020-04-21 13:36:14 +0200

    added disabled to mjs

--- mjs_fs/api_dac.js
+++ mjs_fs/api_dac.js
@@ -1,4 +1,5 @@
 let DAC = {
     enable = ffi('int mgos_dac_output_enable(int)'),
+    disable = ffi('int mgos_dac_output_disable(int)'),
     voltage = ffi('int mgos_dac_output_voltage(int, int)'),
 };
