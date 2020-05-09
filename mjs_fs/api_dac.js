9c0a4d339162dc63ebe47e489b3c633f9a4f2d4d
Francesco Colasante <frank_997@hotmail.it>
2020-05-09 08:18:46 +0200

    fixed mjs

--- mjs_fs/api_dac.js
+++ mjs_fs/api_dac.js
@@ -1,5 +1,8 @@
 let DAC = {
-    enable = ffi('int mgos_dac_output_enable(int)'),
-    disable = ffi('int mgos_dac_output_disable(int)'),
-    voltage = ffi('int mgos_dac_output_voltage(int, int)'),
+    // ## **`DAC.enable(pin)`**
+    // Configure and enable DAC for a `pin`,
+    // return 1 if success, 0 otherwise
+    enable:  ffi('int mgos_dac_output_enable(int)'),
+    disable: ffi('int mgos_dac_output_disable(int)'),
+    voltage: ffi('int mgos_dac_output_voltage(int, int)'),
 };
